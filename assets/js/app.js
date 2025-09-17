3e9afaa9cb527e0e264ae8e61d8b7f55e7fb8c65
Ruslan <kotashov03@gmail.com>
2025-09-18 02:48:03 +0400

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,22 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
+
+    const howSection = document.querySelector(".how");
+
+    if (howSection) {
+        const howItems = howSection.querySelectorAll(".how__item .num");
+        if (howSection && howItems.length > 0) {
+            let current = 0;
+
+            setInterval(() => {
+                howItems.forEach(el => el.classList.remove("active"));
+                howItems[current].classList.add("active");
+                current = (current + 1) % howItems.length;
+            }, 2500);
+        }
+    }
+
+
+
     const heroSlider = new Swiper('.hero__slider-swiper', {
         slidesPerView: 1.5,
         loop: true,
@@ -8,6 +26,47 @@ document.addEventListener('DOMContentLoaded', ()=>{
         autoplay: {
             delay: 5000,
         },
-        
+
     })
+
+    const casesSlider = new Swiper('.cases__items', {
+        slidesPerView: 3,
+        spaceBetween: 24,
+        navigation: {
+            prevEl: '.cases-btn-prev',
+            nextEl: '.cases-btn-next',
+        }
+    })
+
+
+    const faqBtns = document.querySelectorAll('.faq__item button');
+
+    if (faqBtns.length != 0) {
+        faqBtns.forEach(item => {
+            item.addEventListener('click', (e) => {
+                let parent = e.target.closest('.faq__item');
+                let text = parent.querySelector('.text');
+
+                if (parent.classList.contains('active')) {
+                    parent.classList.remove('active');
+                    text.style.maxHeight = 0;
+                } else {
+                    faqBtns.forEach(item => {
+                        item.closest('.faq__item').classList.remove('active');
+                        item.nextElementSibling.style.maxHeight = 0;
+                    })
+                    parent.classList.add('active');
+                    text.style.maxHeight = text.scrollHeight + 'px';
+                }
+            })
+        })
+
+        faqBtns[0].click();
+    }
 })
+
+
+const dynamicAdaptive = new DynamicAdaptive('max');
+dynamicAdaptive.init();
+
+
